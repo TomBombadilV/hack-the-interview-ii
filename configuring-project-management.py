107581a8bb5a9a2a562fde2428b06c8df916e821
Carolyn <carolyngychan@gmail.com>
2020-05-01 10:04:32 -0700

    Styling

--- configuring-project-management.py
+++ configuring-project-management.py
@@ -25,11 +25,14 @@
 from random import randint
 from typing import List
 
-# Method 1
 def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[int]:
+    """
+    Method 1
+    """
     # If there are only 2 students, return empty
     if n < 3: 
         return [-1]
+    
     # Create adjacency matrix
     m = [[0 for _ in range(n)] for _ in range(n)]
     # Add each friendship to the adjacency matrix
@@ -40,6 +43,7 @@ def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[i
             # 1-index to 0-index)
             m[f[0] - 1][f[1] - 1] = 1
             m[f[1] - 1][f[0] - 1] = 1
+    
     # Get list of two's friends
     two_friends = [i for i in range(1, n) if m[1][i] == 1]
     # Go through all of two's friends
@@ -54,15 +58,19 @@ def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[i
                 # If friends with student 1 (index 0), remove edge
                 if m[0][i] == 1:
                     m[0][i] = 0
+    
     # Get list of all of 1's remaining friends (excluding 2 (index 1))
     invites = [i + 1 for i in range(1, n) if m[0][i] == 1 and not(i == 1)]
     return invites if invites else [-1]
 
-# Method 2
 def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[int]:
+    """
+    Method 2
+    """
     twos_friends, twos_friends_friends = set(), set()
     invitees = set()
     i = 0
+    
     # Remove all edges that are invalid (negative or > n)
     while i < len(friendships):
         if friendships[i][0] < 0 or friendships[i][1] < 0 or\
@@ -70,12 +78,14 @@ def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[i
             friendships = friendships[:i] + friendships[i + 1:]
         else:
             i += 1
+    
     # Add all of twos friends to a set
     for f in friendships:
         if f[0] == 2 and not(f[1] == 1) and not(f[1] == 2):
             twos_friends.add(f[1])
         if f[1] == 2 and not(f[0] == 1) and not(f[0] == 2):
             twos_friends.add(f[0])
+    
     # Add all of twos twos friends to a set
     for f in friendships:
         if f[0] in twos_friends and not(f[1] == 1) and not(f[1]) == 2 and \
@@ -84,6 +94,7 @@ def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[i
         if f[1] in twos_friends and not(f[0] == 1) and not(f[1]) == 2 and \
             not(f[0] in twos_friends):
             twos_friends_friends.add(f[0])
+    
     # Add all of ones friends that arent twos friends or twos twos friends to set
     for f in friendships:
         if f[0] == 1 and not(f[1] == 1) and not(f[1] == 2) and \
@@ -92,6 +103,7 @@ def configureProjectPresentation(n: int, friendships: List[List[int]]) -> List[i
         if f[1] == 1 and not(f[0] == 1) and not(f[0] == 2) and \
             not(f[0] in twos_friends) and not(f[0] in twos_friends_friends):
             invitees.add(f[0])
+    
     # Return sorted invitees
     return sorted(list(invitees)) if invitees else [-1]
 
